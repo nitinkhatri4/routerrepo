029b929916161a84f1b7dac46ed92143f11eac06
norem4 <flickeryfucc@gmail.com>
2023-09-05 13:28:34 +0530

    new

---
+++ Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header() {
+  return (
+    <div>
+        <ul className='change'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+  )
+}
---
+++ Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import About from './About'
+import Home from './Home'
+
+export default function Nav() {
+  return (
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Home />}/>
+    <Route path='/about' element={<About />}/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
